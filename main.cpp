6878302e02a6ac9a8c87088bad84a31aae0280e6
Luiz Gustavo Dias Pulz <162385594+LgZika@users.noreply.github.com>
2024-06-01 00:50:51 +0000

    estrutura principal, objetos e funções

--- main.cpp
+++ main.cpp
@@ -3,130 +3,149 @@
 
 using namespace std;
 
-struct var{
+class Task{ // Classe Tasks que contém as informações de cada nota como objetos
+    private: // private, apenas os objetos e métodos da classe podem acessar
+        int idTask, date;
+        string title, description, status;
+
+    public: // public, pode ser acessado em qualquer escopo pelo objeto do tipo da classe
+        Task(int idTask, string title, string description, int date, string status){ // função construtora da classe Task
+            this->idTask = idTask; 
+            this->title = title;
+            this->description = description;
+            this->date = date;
+            this->status = status;
+            // usa-se "this->" para indicar qual é a variável da classe e qual a variável da função
+        }
+        // Notes(int id, string t, string d, int dma, string s) : idTask(id), title(t), description(d), date(dma), status(s) {}
+
+        int getIdTask(){ // função get para retonar o id da tarefa
+            return idTask;
+        }
+
+        void setDate(int date){ // função set para informar valor para a data da tarefa
+            this->date = date;
+        }
+
+        int getDate(){ // função get para retornar a data da tarefa
+            return date;
+        }
+
+        void setDescription(string description){ // função set para informar a descrição da tarefa
+            this->description = description;
+        }
+
+        string getDescription(){ // função get para retornar a descrição da tarefa
+            return description;
+        }
+
+        void setTitle(string title){ // funções get e set para title
+            this->title = title;
+        }  
+
+        string getTitle(){ // função get para retornar o titulo da tarefa
+            return title;
+        }
+
+        void setStatus(string status){ // função set para informa o status da tarefa
+            this->status = status;
+        }
+
+        string getStatus(){ // função get para retornar o status da tarefa
+            return status;
+        }
 };
 
-class Task{ //Classe notas que contém as informações de cada nota como objetos
-private: // private, apenas a classe pode acessar
-    int idTask, date;
-    string title, description, status;
-
-public: // public, pode ser acessado em qualquer escopo
-    // Notes(int id, string t, string d, int dma, string s) : idTask(id), title(t), description(d), date(dma), status(s) {}
-    Task(int idTask, string title, string description, int date, string status){
-        this->idTask = idTask; // usa-se "this->" para indicar qual é a variável da classe e qual a variável da função
-        this->title = title;
-        this->description = description;
-        this->date = date;
-        this->status = status;
-    }
-    
-    // função get para idTask
-    int getIdTask(){
-        return idTask;
-    }
-
-    // funções get e set para date
-    void setDate(int date){
-        this->date = date;
-        }
-    int getDate(){
-        return date;
-    }
-
-    // funções get e set para descrption
-    void setDescription(string description){
-        this->description = description;
-        }
-    string getDescription(){
-        return description;
-    }
-
-    // funções get e set para title
-    void setTitle(string title){
-        this->title = title;
-        }
-    string getTitle(){
-        return title;
-    }
-
-    // funções get e set para status
-    void setStatus(string status){
-        this->status = status;
-        }
-    string getStatus(){
-        return status;
-    }
-};
-
-void createTask(vector<Task>& task){
-    int *idTask = new int;
+void createTask(vector<Task>& task){ // função para adicionar tarefas
+    int *idTask = new int(task.size());
     int *date = new int;
     string *title = new string;
     string *description = new string;
     string *status = new string;
 
-    cout << "Id: ";
-    cin >> *idTask;
+    cout << "Add new Task" << endl;
+    cout << "ID: " << *idTask << endl;
     cout << "Title: ";
-    cin >> *title;
+    cin >> *title; 
     cout << "Description: ";
     cin >> *description;
-    cout << "Date: ";
+    cout << "Deadline: ";
     cin >> *date;
-    cout << "Status: ";
-    cin >> *status;
+    while(true){
+        cout << "Status (1 - Pending, 2 - In Progress, 3 - Completed): ";
+        cin >> *status;
+        if(*status == "1"){
+            *status = "Pending";
+            break;
+        }else if(*status == "2"){
+            *status = "In Progress";
+            break;
+        }else if(*status == "3"){
+            *status = "Completed";
+            break;
+        }else{
+            cout << "Invalid Status!" << endl;
+        }
+    }
+    cout << "Task added successfully!" << endl;
 
     task.push_back(Task(*idTask, *title, *description, *date, *status));
 
     delete idTask, date, title, description, status;
 }
-void editTask(){
+void editTask(vector<Task>& task){
     cout << "Voce escolheu a opcao 2." << endl;    
 }
-void deleteTask(){
+void deleteTask(vector<Task>& task){
     cout << "Voce escolheu a opcao 3." << endl;
 }
-void searchTask(){
+void searchTask(vector<Task>& task){
     cout << "Voce escolheu a opcao 4." << endl;
 }
-void filterTask(){
+void filterTask(vector<Task>& task){
     cout << "Voce escolheu a opcao 5." << endl;
 }
+
+void showTask(vector<Task>& task){ // função para exibir as tarefas
+    for (size_t i = 0; i < task.size(); i++){
+        cout << "ID: " << task[i].getIdTask() << endl;
+        cout << "Title: " << task[i].getTitle() << endl;
+        cout << "Description: " << task[i].getDescription() << endl;
+        cout << "Deadline: " << task[i].getDate() << endl;
+        cout << "Status: " << task[i].getStatus() << endl;
+        cout << endl;
+    }
+}
+
 int main(){
-    vector<Task> data;
-
-    // data.push_back(Task(1, "Trabalho01", "tem que ser feito", 1092004,"fazendo"));
-    // data.push_back(Notes("Mundo"));
-
-    createTask(data);
-
-    cout << data[0].getIdTask() << endl;
-    cout << data[0].getTitle() << endl;
-    cout << data[0].getDescription() << endl;
-    cout << data[0].getDate() << endl;
-    cout << data[0].getStatus() << endl;
-
-/*
+    vector<Task> task; // cria um vetor em que cada posição é um objeto da classe Task
     int choice;
 
-    do{
-        cout << "=== Menu ===" << endl;
-        cout << "1. Opcao 1" << endl;
-        cout << "2. Opcao 2" << endl;
-        cout << "3. Opcao 3" << endl;
-        cout << "4. Sair" << endl;
-        cout << "Escolha uma opcao: ";
+    do{ 
+        // Exibição de menu padrão no terminal
+        cout << "Task Manager System" << endl;
+        cout << "1. Add Tasks" << endl;
+        cout << "2. Show Tasks "<< endl;
+        cout << "3. Edit Tasks "<< endl;
+        cout << "4. Delete Tasks" << endl;
+        cout << "5. Search Tasks" << endl;
+        cout << "6. Filter Tasks by Status" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Choose an option: "; 
         cin >> choice;
+        cout << endl;
 
         // switch case para as opcoes do menu
         switch (choice){
-            case 1: opcaoUm(); break;
-            case 2: opcaoDois(); break;
-            case 3: opcaoTres(); break;
-            default: cout << "Opcao Invalida. Tente novamente." << endl; break;
+            case 0: return false; break;
+            case 1: createTask(task); break;
+            case 2: showTask(task); break;
+            case 3: editTask(task); break;
+            case 4: deleteTask(task); break;
+            case 5: searchTask(task); break;
+            case 6: filterTask(task); break;
+            default: cout << "Invalid option. Try again." << endl; break;
         }
-    }while(choice != 6);
-    */
+    }while(true);
     return 0;
 }
