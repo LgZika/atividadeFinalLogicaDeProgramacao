852d58fde4ab194d4d168716eabbff8d80681f6c
LgZika <luizgustavodiaspulzoficial@gmail.com>
2024-06-01 23:30:28 -0300

    atualização da formatação no terminal

--- main.cpp
+++ main.cpp
@@ -303,15 +303,25 @@ int main(){
     do{ 
         // Exibição de menu padrão no terminal
         cout << endl;
-        cout << "Task Manager System" << endl;
-        cout << "1. Add Tasks" << endl;
-        cout << "2. Show Tasks "<< endl;
-        cout << "3. Edit Tasks "<< endl;
-        cout << "4. Delete Tasks" << endl;
-        cout << "5. Search Tasks" << endl;
-        cout << "6. Filter Tasks by Status" << endl;
-        cout << "0. Exit" << endl;
-        cout << "Choose an option: "; 
+        cout << (char)201;
+        for(size_t i = 0; i < 31; i++){
+            cout << (char)205;
+        }
+        cout << (char)187 << endl;
+        cout << (char)186 << "      Task Manager System      " << (char)186 << endl;
+        cout << (char)186 << " 1. Add Tasks                  " << (char)186 << endl;
+        cout << (char)186 << " 2. Show Tasks                 " << (char)186 << endl;
+        cout << (char)186 << " 3. Edit Tasks                 " << (char)186 << endl;
+        cout << (char)186 << " 4. Delete Tasks               " << (char)186 << endl;
+        cout << (char)186 << " 5. Search Tasks               " << (char)186 << endl;
+        cout << (char)186 << " 6. Filter Tasks by Status     " << (char)186 << endl;
+        cout << (char)186 << " 0. Exit                       " << (char)186 << endl;
+        cout << (char)200;
+        for(size_t i = 0; i < 31; i++){
+            cout << (char)205;
+        }
+        cout << (char)188 << endl;
+        cout << "Choose an option: ";  
         cin >> *choice;
         cout << endl;
         
--- testesEAfins/testeFormatacao.cpp
+++ testesEAfins/testeFormatacao.cpp
@@ -3,6 +3,25 @@
 using namespace std;
 
 int main(){
-    cout << (char)186 << endl;
+    cout << (char)201;
+    for(size_t i = 0; i < 31; i++){
+        cout << (char)205;
+    }
+    cout << (char)187 << endl;
+    cout << (char)186 << "      Task Manager System      " << (char)186 << endl;
+    cout << (char)186 << " 1. Add Tasks                  " << (char)186 << endl;
+    cout << (char)186 << " 2. Show Tasks                 " << (char)186 << endl;
+    cout << (char)186 << " 3. Edit Tasks                 " << (char)186 << endl;
+    cout << (char)186 << " 4. Delete Tasks               " << (char)186 << endl;
+    cout << (char)186 << " 5. Search Tasks               " << (char)186 << endl;
+    cout << (char)186 << " 6. Filter Tasks by Status     " << (char)186 << endl;
+    cout << (char)186 << " 0. Exit                       " << (char)186 <<endl;
+    cout << (char)200;
+    for(size_t i = 0; i < 31; i++){
+        cout << (char)205;
+    }
+    cout << (char)188 << endl;
+    cout << "Choose an option: "; 
+    
     return 0;
 }
